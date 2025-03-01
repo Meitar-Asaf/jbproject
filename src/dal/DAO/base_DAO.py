26dc20acdf22d3e94c0f2897361b911d12427575
Meitar-Asaf <meitarasaf@gmail.com>
2025-03-01 20:47:48 +0200

    first version of base_DAO base_print_all, base_add, base_delete

--- src/dal/DAO/base_DAO.py
+++ src/dal/DAO/base_DAO.py
@@ -11,24 +11,42 @@ class BaseDAO:
         try:
             with pg.connect(self.connection_details) as connection:
                 with connection.cursor() as cursor:
-                    cursor.execute(query, params)
+                    if params:
+                        cursor.execute(query, params if params else ())
                     connection.commit()
+                    if query.startswith('SELECT'):
+                        results = cursor.fetchall()
+                        for result in results:
+                                print(result)
+            
+                        
         except pg.DatabaseError as e:
             print("Database error: {}".format(e))
         except Exception as e:
             print("Unexpected error: {}".format(e))
 
-    def base_update(self, table_name: str, columns: tuple, values: tuple, condition: str, id: str):
+    def base_update(self, table_name: str, columns: tuple, values: tuple, id_column_name: str, id: str):
         if len(columns) != len(values):
             raise ValueError("Number of columns and values must match")
 
         # Creating the SET clause dynamically
         columns = ', '.join(
-            [f"{columns[i]} = %s" for i in range(len(columns))])
+            [f'{columns[i]} = %s' for i in range(len(columns))])
 
         # Constructing the full SQL query
-        query = 'UPDATE ' + table_name + 'SET ' + columns + ' WHERE ' + condition
+        query = 'UPDATE ' + table_name + 'SET ' + columns + ' WHERE ' + id_column_name + ' = %s;'
         params = values + id
         self.connect_and_change_table(query, params)
 
-    def base_add(self, table_name, )
+    def base_add(self, table_name:str, columns:tuple, values:tuple):
+        place_holders = '%s ,' * (len(values) - 1) + '%s'
+        query = f'INSERT INTO {table_name}({', '.join(columns)}) VALUES({place_holders});'
+        self.connect_and_change_table(query, values)
+
+    def base_print_all(self, table_name):
+        query = f'SELECT * FROM {table_name};'
+        self.connect_and_change_table(query, )
+
+    def base_delete(self, table_name:str, id_column_name:str, id:str):
+        query = f'DELETE FROM {table_name} WHERE {id_column_name} = %s'
+        self.connect_and_change_table(id)
--- src/dal/DAO/roles_dao.py
+++ src/dal/DAO/roles_dao.py
@@ -4,6 +4,10 @@ import psycopg as pg
 
 class RolesDAO(BaseDAO):
     def update(self, columns: tuple, values: tuple, role_id:str):
-        condition = 'role_id = '
-        BaseDAO.base_update('roles', columns, values,condition, role_id)
+        super().base_update('roles', columns, values,'role_id', role_id)
+    def add(self, columns:tuple, values:tuple):
+        super().base_add('roles',columns, values)
+    def print_all(self):
+        super().base_print_all('roles')
+    def
 
